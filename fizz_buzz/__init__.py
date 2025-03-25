954397eb0d1d94356648c1e0048ce234669241ae
jeremias-mena <menajeremias08@gmail.com>
2025-03-25 18:04:57 -0300

    update(__init__): add new module config

--- fizz_buzz/__init__.py
+++ fizz_buzz/__init__.py
@@ -1,5 +1,7 @@
+from .config import Config
 from .utils import Util
 from .model import FizzBuzzNN
 
-__all__ = ['Util',
-           'FizzBuzzNN']
+__all__ = ['Config',
+           'Util',
+           'FizzBuzzNN',]
