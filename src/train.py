c1772ab07d8724d51fc555c174c2657f679b65a1
jeremias-mena <menajeremias08@gmail.com>
2025-03-26 15:46:57 -0300

    update(train): change path to save trained model

--- src/train.py
+++ src/train.py
@@ -2,6 +2,8 @@ import pickle
 import random
 from fizz_buzz import FizzBuzzNN, Util, Config
 
+dir_path = './src/model'
+
 num_hidden = 30
 
 x = [Util().binary_encoder(i) for i in range(101, 1024)]
@@ -13,7 +15,9 @@ fb_network = FizzBuzzNN(neural_network=[
                                         ])
 fb_network.train(x, y)
 
-with open(Config.train_model_path, "wb") as model_file:
+Config().create_dir(dir_path=dir_path)
+
+with open(dir_path + '/fizz_buzz_NN.pkl', "wb") as model_file:
     pickle.dump(fb_network, model_file)
 
 
